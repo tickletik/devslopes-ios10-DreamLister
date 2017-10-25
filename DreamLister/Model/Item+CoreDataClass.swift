66fd20d3306c7e6d75fa7cdd2fb54abad2917cfd
Ronny Abraham <this.ronny@gmail.com>
2017-10-25 16:06:36 +0300

    override awakeFromInsert and set created to current date

--- DreamLister/Model/Item+CoreDataClass.swift
+++ DreamLister/Model/Item+CoreDataClass.swift
@@ -12,5 +12,10 @@ import CoreData
 
 @objc(Item)
 public class Item: NSManagedObject {
+    
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+        self.created = NSDate()
+    }
 
 }
