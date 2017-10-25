eb4abf9a8640b43a1c73c7346b428810908105ef
Ronny Abraham <this.ronny@gmail.com>
2017-10-25 16:08:32 +0300

    rename ViewController to MainVC

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by ronny abraham on 10/25/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
