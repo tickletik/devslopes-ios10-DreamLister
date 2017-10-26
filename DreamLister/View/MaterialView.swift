6bd8037820f8e727626b8440ef88acee75516ff6
Ronny Abraham <this.ronny@gmail.com>
2017-10-26 12:45:01 +0300

    add new MaterialView class to project

---
+++ DreamLister/View/MaterialView.swift
@@ -0,0 +1,14 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by ronny abraham on 10/26/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+
+}
