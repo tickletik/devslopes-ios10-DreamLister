0790232f121e000a378cbbdd30fe4f3fa45bb335
Ronny Abraham <this.ronny@gmail.com>
2017-10-30 22:16:19 +0200

    create iboutlets from itemcell

--- DreamLister/View/ItemCell.swift
+++ DreamLister/View/ItemCell.swift
@@ -10,15 +10,8 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
 }
