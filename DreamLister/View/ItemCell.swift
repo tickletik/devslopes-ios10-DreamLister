e4110f1ada2e2bb41bd5e94e5fb7ade6a296edf3
Ronny Abraham <this.ronny@gmail.com>
2017-10-30 22:16:04 +0200

    add ItemCell to project

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by ronny abraham on 10/30/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
