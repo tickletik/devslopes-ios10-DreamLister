3f6ca6670ab83b75e6fd365fec3550200953aea9
Ronny Abraham <this.ronny@gmail.com>
2017-10-30 22:26:58 +0200

    fix nsfetchedresultscontroller to delegate

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segment: UISegmentedControl!
