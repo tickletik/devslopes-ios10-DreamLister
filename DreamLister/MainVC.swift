dc498824ecdfc64dae01f6005e3a4407e2a6423d
Ronny Abraham <this.ronny@gmail.com>
2017-10-30 22:21:19 +0200

    add some delegat and datasource methods to MainVC

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -20,9 +20,16 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
     }
 
 
